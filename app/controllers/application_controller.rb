e3b7a3174d2c46d926d7a4f4b365a5072f1d327f
Tiffany Chong <sciffany@gmail.com>
2018-07-29 00:11:13 +0800

    create controller for courses

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,19 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
     devise_parameter_sanitizer.permit(:account_update, keys: [:name])
   end
+
+  def ensure_params_fields(fields)
+    all_present = fields.reduce(true) do |acc, field|
+      params[field].present? && acc
+    end
+    unless all_present
+      render_error('Missing parameter', :unprocessable_entity)
+      return
+    end
+    true
+  end
+
+  def render_error(message)
+    @message = message
+  end
 end
--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,5 +1,41 @@
 # frozen_string_literal: true
 
 class CoursesController < ApplicationController
-  def index; end
+  # /courses - lists all course objects
+  def index
+    courses_by_university = Course
+                            .select(:id, :university_id, :code)
+                            .where(university_id: current_user.university_id)
+    if params[:code]
+      course = courses_by_university.find_by(code: params[:code].upcase)
+      redirect_to course_semesters_url course_id: course.id if course
+    else
+      @courses = courses_by_university
+    end
+  end
+
+  def show
+    courses_by_university = Course
+                            .select(:id, :university_id, :code)
+                            .where(university_id: current_user.university_id)
+
+    @course = courses_by_university.find_by(id: params[:id])
+  end
+
+  def create
+    return unless ensure_params_fields([:code])
+    course = Course.new course_params
+    course.university_id = current_user.university_id
+    if course.save
+      @course = course.slice(:id, :code)
+    else
+      render_error(course.errors.full_messages.join(', '), :bad_request)
+    end
+  end
+
+  private
+
+  def course_params
+    params.require(:course).permit(:code)
+  end
 end
