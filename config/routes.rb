60748bd3d28229ad52173ee518d97a081155e63d
Julius Putra Tanu Setiaji <indocomsoft@gmail.com>
2018-07-28 22:22:53 +0800

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,15 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'courses#index'
 
-  resources :courses
+  resources :courses, only: %i[index new create] do
+    resources :semesters, shallow: true, only: %i[index new create] do
+      resources :papers, shallow: true, only: %i[index new create] do
+        resources :questions, shallow: true, only: %i[index new create] do
+          resources :answers, shallow: true, only: %i[index new create] do
+            resources :comments, shallow: true, only: %i[index new create]
+            resources :votes, shallow: true
+        end
+      end
+    end
+  end
 end
