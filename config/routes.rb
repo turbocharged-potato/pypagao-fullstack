ba967695b5370006db48f4f431d5714040692a2b
Julius Putra Tanu Setiaji <indocomsoft@gmail.com>
2018-07-29 23:39:32 +0800

    Implement Questions (#5)
    * Implement Questions
    
    * Fix linter offences

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'univerities#index'
+  root to: 'universities#index'
 
   resources :users, only: %i[show index]
   resources :universities, only: %i[index create] do
