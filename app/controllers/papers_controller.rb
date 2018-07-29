6e36256aa65e91c646893ed9a76c17956b1faefc
Tiffany Chong <sciffany@gmail.com>
2018-07-29 11:52:00 +0800

    papers forms

--- app/controllers/papers_controller.rb
+++ app/controllers/papers_controller.rb
@@ -6,6 +6,24 @@ class PapersController < ApplicationController
     return unless ensure_params_fields([:semester_id])
     @semester = Semester.find_by(id: params[:semester_id])
     @course = @semester.course
-    @papers = Paper.all
+    @papers = @semester.papers
+    @paper = Paper.new
+  end
+
+  def create
+    paper = Paper.new paper_params
+    if paper.save
+      redirect_to semester_papers_url(paper.semester.course.id),
+                  notice: "New paper created under #{paper.semester.course.code} #{paper.semester.start_year}/ #{paper.semester.start_year} Sem #{paper.semester.number} "
+    else
+      redirect_to semester_papers_url(paper.semester.course.id),
+                  alert: "Failed to create paper. #{paper.errors.full_messages.join(', ')}"
+    end
+  end
+
+  private
+
+  def paper_params
+    params.require(:paper).permit(:name, :semester_id)
   end
 end
