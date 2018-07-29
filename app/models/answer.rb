2c96625161ac61c4a0d6ea295fb8a71d5f746db2
Julius Putra Tanu Setiaji <indocomsoft@gmail.com>
2018-07-29 22:34:30 +0800

    Change to counter_cache

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -24,15 +24,17 @@
 #
 
 class Answer < ApplicationRecord
-  default_scope { eager_load(:upvotes, :downvotes, :comments, :user) }
+  default_scope { eager_load(:user) }
 
   belongs_to :question
   belongs_to :user
-  has_many :comments, dependent: :destroy
+  has_many :comments, dependent: :destroy, counter_cache: true
   has_many :votes, dependent: :destroy
 
-  has_many :upvotes, -> { where score: 1 }, class_name: 'Vote', inverse_of: :answer
-  has_many :downvotes, -> { where score: -1 }, class_name: 'Vote', inverse_of: :answer
+  has_many :upvotes, -> { where score: 1 }, class_name: 'Vote', inverse_of: :answer,
+                                            counter_cache: true
+  has_many :downvotes, -> { where score: -1 }, class_name: 'Vote', inverse_of: :answer,
+                                               counter_cache: true
 
   validates :content, presence: true
 
