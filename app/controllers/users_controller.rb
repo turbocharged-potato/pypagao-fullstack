910bae67bb6d88f99da27811cc1d8425a4ba9d60
Tiffany Chong <sciffany@gmail.com>
2018-07-29 12:55:16 +0800

    add user resource

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+class UsersController < ApplicationController
+  # /users - lists all users
+  def index
+    @users = User.all #select(:id, :university_id, :name)
+    # @users = if params[:university_id]
+    # 				    users.where(university_id: params[:university_id])
+  		# 			 else
+  		# 					users.where(name: params[:name]) if params[:name]
+  		# 			 end
+  end
+
+  def show
+  	@user = User.find_by(id: params[:id])
+  #   user[:votes] = get_score(params[:id])
+  #   render_json(user, :ok)
+
+ 	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'courses#index'
 
+  resources :users
+  
   resources :courses, only: %i[index new create] do
     resources :semesters, shallow: true, only: %i[index new create] do
       resources :papers, shallow: true, only: %i[index new create] do
