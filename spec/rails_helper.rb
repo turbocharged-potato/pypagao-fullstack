987ffc8fd74042f0038420353b53377d0d882b5c
Julius Putra Tanu Setiaji <indocomsoft@gmail.com>
2018-07-30 11:10:16 +0800

    Add devise rspec-rails helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -79,3 +79,8 @@ end
 RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
 end
+
+RSpec.configure do |config|
+  config.include Devise::Test::ControllerHelpers, type: :controller
+  config.include Devise::Test::ControllerHelpers, type: :view
+end
