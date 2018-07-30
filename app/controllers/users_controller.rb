f181603f9bcd3c312054f4721f513d082b679adf
Julius Putra Tanu Setiaji <indocomsoft@gmail.com>
2018-07-30 22:33:51 +0800

    Change to show top 100 contributors and show 20 recent answers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,11 +3,11 @@
 class UsersController < ApplicationController
   # /users - lists all users
   def index
-    @users = User.eager_load(:university).all.sort { |a, b| b.karma <=> a.karma }.take(10)
+    @users = User.eager_load(:university).all.sort { |a, b| b.karma <=> a.karma }.take(100)
   end
 
   def show
     @user = User.find(params[:id])
-    @answers = Answer.where(user_id: @user.id).order(:created_at)
+    @answers = Answer.where(user_id: @user.id).order(:created_at).limit(20)
   end
 end
